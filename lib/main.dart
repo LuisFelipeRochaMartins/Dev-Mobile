7d2e1a3ccf99b88e72fb703aecd36d1ea38c151e
Luís Felipe Rocha Martins <luisfelipetochamartins>
2023-10-23 10:42:42 -0300

    Creating application layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ExemploInicial());
+  runApp(MaterialApp(
+    title: 'Contatos',
+    initialRoute: '/',
+    theme: ThemeData.dark(),
+    routes: {
+      '/'        : (context) => const Contatos(),
+      '/cadastro': (context) => const Cadastro(),
+    },
+  ));
 }
 
-class ExemploInicial extends StatelessWidget {
+class Contatos extends StatelessWidget {
+  const Contatos({super.key});
 
+  @override
   Widget build(BuildContext context) {
-    final perguntas = ['Você possui CNH?', 'Você possui carro?'];
-    return MaterialApp(
-      home: Scaffold (
-        appBar: AppBar(
-          title: const Text('tester'),
-        ),
-        body: Column (
-          children: [
-            Row(children: [
-            Text(perguntas[0]),
-            const ElevatedButton(onPressed: null, child: Text('Sim')),
-            const ElevatedButton(onPressed: null, child: Text('Não')),
-          ],)
-        ],)
-            
-          
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contatos'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_call),
+            tooltip: "Adicionar novo Contato",
+            onPressed: (() => Navigator.pushNamed(context, '/cadastro')),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Cadastro extends StatefulWidget {
+  const Cadastro({Key? key}) : super(key: key);
+
+  @override
+  _CadastroState createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cadastro'),
+      ),
+      body: Stepper(
+        currentStep: _index,
+        type: StepperType.vertical,
+        onStepCancel: () {
+          if (_index > 0) {
+            setState(() {
+              _index -= 1;
+            });
+          }
+        },
+        onStepContinue: () {
+          if (_index <= 0) {
+            setState(() {
+              _index += 1;
+            });
+          }
+        },
+        onStepTapped: (int index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        steps: <Step>[
+          Step(
+            title: const Text('Ola '),
+            content: Container(
+              alignment: Alignment.centerLeft,
+              child: const Text('Testing'),
+            ),
+          ),
+          const Step(
+            title: Text('Step 2 title'),
+            content: Text('Content for Step 2'),
+          ),
+        ],
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:primeira_aula/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ExemploInicial());
+    await tester.pumpWidget(Contato());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
